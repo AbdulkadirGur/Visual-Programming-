5713d7991d36c9f9a3f02945cf5a490422d9265c
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-10-31 22:29:38 +0300

    Basic form exercises

---
+++ Course1/Course1/UserControl1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course1
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Click olayindan gelen yazi";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Abdulkadir";
+           // MessageBox.Show("Merhaba");
+
+           
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
