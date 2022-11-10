21fe578ddbf74692a5e34daa386116c164d5317f
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-11-10 05:56:42 +0300

    Form exercise

--- Course2/Course2/Form1.cs
+++ Course2/Course2/Form1.cs
@@ -16,5 +16,26 @@ namespace Course2
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Abdulkadir" && textBox2.Text=="123")
+            {
+                //Form2 form2 = new Form2(); 
+                Form2 form2 = new Form2(this);
+                //this.Visible = false;
+                 form2.Show();
+               //form2.ShowDialog(); // if you don't close this window you can't open other window
+            }
+            else
+            {
+                MessageBox.Show("Please enter the right password or username !!!");
+            }
+        }
     }
 }
---
+++ Course2/Course2/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course2
+{
+    public partial class Form2 : Form
+    {
+        Form1 god;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form2(Form1 x)
+        {
+            // called to form1
+            InitializeComponent();
+            this.god = x;
+            x.button1.Text = "Form2'den yazildi";
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Button in Form2 clicked ";
+            god.button1.Text = "F2_Button";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Dispose ();
+            god.Dispose();
+        }
+    }
+}
