b669634760aba1d869ce09235930c0bf074ae344
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-11-25 12:05:49 +0300

    Add to new Search and change button

---
+++ Course3/Course3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Course3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Bul = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(319, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "and";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(319, 99);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 30);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "abdul";
+            // 
+            // textBox3
+            // 
+            this.textBox3.HideSelection = false;
+            this.textBox3.Location = new System.Drawing.Point(241, 151);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(300, 160);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "I have coding and programming talents in addition to an engineer\'s education from" +
+    " the university. Design, business concepts, and new ventures pique my interest.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 67);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Bul
+            // 
+            this.Bul.AutoSize = true;
+            this.Bul.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Bul.Location = new System.Drawing.Point(256, 40);
+            this.Bul.Name = "Bul";
+            this.Bul.Size = new System.Drawing.Size(40, 25);
+            this.Bul.TabIndex = 4;
+            this.Bul.Text = "Bul";
+            this.Bul.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Bul.Click += new System.EventHandler(this.Bul_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(236, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Degistir";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 67);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Change";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(241, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 67);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Next to Search";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(404, 395);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 67);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Next to Change";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(295, 468);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(168, 67);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Change to All";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 578);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Bul);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Bul;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Course3/Course3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bul_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int uzunluk = textBox1.Text.Length;
+            try
+            {
+                int bas = textBox3.Text.IndexOf(textBox1.Text);
+                textBox3.Select(bas, uzunluk);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bulunamadi!!!");
+              
+            }
+           
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox3.SelectedText = textBox2.Text;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Herhangi bir alani secmediniz");
+                throw;
+            }
+            
+            
+            
+        }
+    }
+}
